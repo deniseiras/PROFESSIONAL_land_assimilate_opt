37b837281cf409bc7980c8c75774cb864b3e802c
deniseiras <denis.eiras@gmail.com>
2025-03-11 16:10:39 +0100

    updater

--- update_TESTGSWP.bash
+++ update_TESTGSWP.bash
@@ -7,14 +7,14 @@ S='/work/cmcc/de34824/work/PROFESSIONAL_land_assimilate_opt'
 
 
 cd $W
-rm -f assimilate.csh 
-ln -sf $S/assimilate.csh 
+rm -f assimilate.csh assimilate_executor.csh
+cp -f $S/assimilate.csh $S/assimilate_executor.csh .
 
 cd $W/run
 runfiles='run_dart_to_clm.bash run_filter.bash run_clm_to_dart_par.bash run_dart_to_clm_snow.bash run_inflation.bash'
 rm -f $runfiles
 
 for f in $runfiles; do
-  ln -sf $S/run/$f .
+  cp -f $S/run/$f .
 done
 
