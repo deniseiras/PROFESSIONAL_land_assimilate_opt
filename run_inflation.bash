c2d8b841673734d7faee669eb6802b64fb2e6c1e
deniseiras <denis.eiras@gmail.com>
2025-05-12 11:29:46 +0200

    updates adding

---
+++ run_inflation.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+
+# TODO check / improve dir
+source /users_home/cmcc/lg07622/modules_juno.me
+./fill_inflation_restart || exit 4
+
--- update_TESTGSWP.bash
+++ update_TESTGSWP.bash
@@ -2,19 +2,23 @@
 set -x
 # switch_user spreads-lnd
 
-W='/work/cmcc/spreads-lnd/work_d4o/TEST_GSWP'
-S='/work/cmcc/de34824/work/PROFESSIONAL_land_assimilate_opt'
+WORK_DIR=$1
+SOURCE_DIR=$PWD
 
 
-cd $W
+cd $WORK_DIR
 rm -f assimilate.csh assimilate_executor.csh
-cp -f $S/assimilate.csh $S/assimilate_executor.csh .
+cp -f $SOURCE_DIR/assimilate.csh $SOURCE_DIR/assimilate_executor.csh .
 
-cd $W/run
+cd $WORK_DIR/run
 runfiles='run_dart_to_clm.bash run_filter.bash run_clm_to_dart_par.bash run_dart_to_clm_snow.bash run_inflation.bash'
 rm -f $runfiles
 
 for f in $runfiles; do
-  cp -f $S/run/$f .
+  cp -f $SOURCE_DIR/run/$f .
 done
+
+
+
+
 
